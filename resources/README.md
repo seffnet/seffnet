fda551064d67a58e3771887f703090a64f4e3a21
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2019-04-25 14:31:59 +0200

    add files to create package

---
+++ resources/README.md
@@ -0,0 +1,6 @@
+## sider_graph.pickle
+a graph created from sider database
+## chem_sim_graph.pickle
+a graph created from chemicals in sider database
+Nodes: Chemicals
+Edges: association
--- src/SE_KGE/chemical_similarities.py
+++ src/SE_KGE/chemical_similarities.py
@@ -2,10 +2,12 @@ from rdkit import Chem
 from rdkit.Chem import MACCSkeys
 from rdkit import DataStructs
 import itertools as itt
-from tqdm import tqdm_notebook as tqdm
+import tqdm
 import urllib.request
 import urllib
 from urllib.error import HTTPError
+from pybel import BELGraph
+import pybel
 
 
 def getresult(url):
@@ -33,7 +35,7 @@ def CidToSmiles(cid):
 
 def SmilesToCid(smiles):
     """
-    gets the chemical pubmed ID from the SMILES
+    gets the chemical pubchem ID from the SMILES
     :param smiles: the SMILES code of a chemical
     :return: the pubchem ID
     """
@@ -43,26 +45,37 @@ def SmilesToCid(smiles):
 def get_similarity(chemicals_list):
     """
     gets the similarities between all pair combinations of chemicals in the list
-    :param chemicals_list: a list of chemicals as pubmed ID
+    :param chemicals_list: a list of chemicals as pubchem ID
     :return: a dictionary with the pair chemicals as key and similarity calculation as value
     """
     smiles_dict = {}
-    for drug in tqdm(drug_list):
-        smiles = CidToSmiles(drug)
+    for chemical in tqdm(chemicals_list, desc='Getting SMILES'):
+        smiles = CidToSmiles(chemical)
         if type(smiles) != str:
             smiles = smiles.decode("utf-8")
-        smiles_dict[drug] = smiles
-    return smiles_dict
+        smiles_dict[chemical] = smiles
     ms = {}
-    for pubchem, smiles in pubchem_to_smiles.items():
+    for pubchem, smiles in tqdm(smiles_dict.items(), desc='Creating molecules'):
         ms[pubchem] = Chem.MolFromSmiles(smiles)
     fps = {}
-    for pubchem_id, mol in tqdm(ms.items()):
+    for pubchem_id, mol in tqdm(ms.items(), desc='Creating fingerprints'):
         if mol == None:
             continue
         fps[pubchem_id] = MACCSkeys.GenMACCSKeys(mol)  # using MACCS
     chem_sim = {
         (pubchem_id_1, pubchem_id_2): DataStructs.FingerprintSimilarity(mol_1, mol_2)
-        for (pubchem_id_1, mol_1), (pubchem_id_2, mol_2) in tqdm(itt.combinations(fps.items(), 2))
+        for (pubchem_id_1, mol_1), (pubchem_id_2, mol_2) in tqdm(itt.combinations(fps.items(), 2), desc='Calculating Similarities')
     }
     return chem_sim
+
+def create_similarity_graph(chemicals_list, name = '', version = '1.1.0', authors = '', contact = '', description =''):
+    """ Creates a BELGraph with chemicals as nodes, and similarity as edges
+    :param chemicals_list: a list of chemicals as pubchem ID
+    :return: BELGraph"""
+    chem_sim = get_similarity(chemicals_list)
+    chem_sim_graph = BELGraph(name, version, description, authors, contact)
+    for (pubchem_1, pubchem_2), sim in tqdm(chem_sim.items(), desc='Creating BELGraph'):
+        if sim < 0.5 :
+            continue
+        chem_sim_graph.add_unqualified_edge(pybel.dsl.Abundance(namespace='pubchem', name=pubchem_1), pybel.dsl.Abundance(namespace='pubchem', name=pubchem_2), 'similar_to')
+    return chem_sim_graph
