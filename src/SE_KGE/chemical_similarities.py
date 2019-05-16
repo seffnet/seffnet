dd02423015152759c1c38ad84c731dd148b5d2b0
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2019-05-17 00:42:55 +0200

    add clustering notebook

--- src/SE_KGE/chemical_similarities.py
+++ src/SE_KGE/chemical_similarities.py
@@ -1,12 +1,12 @@
+import itertools as itt
 from rdkit import Chem
 from rdkit.Chem import MACCSkeys
 from rdkit import DataStructs
-import itertools as itt
 import tqdm
 import urllib.request
 import urllib
 from urllib.error import HTTPError
-from pybel import BELGraph
+
 import pybel
 
 
@@ -54,28 +54,31 @@ def get_similarity(chemicals_list):
         if type(smiles) != str:
             smiles = smiles.decode("utf-8")
         smiles_dict[chemical] = smiles
-    ms = {}
-    for pubchem, smiles in tqdm(smiles_dict.items(), desc='Creating molecules'):
-        ms[pubchem] = Chem.MolFromSmiles(smiles)
-    fps = {}
-    for pubchem_id, mol in tqdm(ms.items(), desc='Creating fingerprints'):
-        if mol == None:
-            continue
-        fps[pubchem_id] = MACCSkeys.GenMACCSKeys(mol)  # using MACCS
+    fps = get_fingerprints(smiles_dict)
     chem_sim = {
         (pubchem_id_1, pubchem_id_2): DataStructs.FingerprintSimilarity(mol_1, mol_2)
-        for (pubchem_id_1, mol_1), (pubchem_id_2, mol_2) in tqdm(itt.combinations(fps.items(), 2), desc='Calculating Similarities')
+        for (pubchem_id_1, mol_1), (pubchem_id_2, mol_2) in
+        tqdm(itt.combinations(fps.items(), 2), desc='Calculating Similarities')
     }
     return chem_sim
 
-def create_similarity_graph(chemicals_list, name = '', version = '1.1.0', authors = '', contact = '', description =''):
+
+def get_fingerprints(chemicals_dict):
+    ms = {}
+    for pubchem, smiles in tqdm(chemicals_dict.items(), desc='Getting fingerprints'):
+        ms[pubchem] = MACCSkeys.GenMACCSKeys(Chem.MolFromSmiles(smiles))
+    return ms
+
+
+def create_similarity_graph(chemicals_list, name='', version='1.1.0', authors='', contact='', description=''):
     """ Creates a BELGraph with chemicals as nodes, and similarity as edges
     :param chemicals_list: a list of chemicals as pubchem ID
     :return: BELGraph"""
     chem_sim = get_similarity(chemicals_list)
-    chem_sim_graph = BELGraph(name, version, description, authors, contact)
+    chem_sim_graph = pybel.BELGraph(name, version, description, authors, contact)
     for (pubchem_1, pubchem_2), sim in tqdm(chem_sim.items(), desc='Creating BELGraph'):
-        if sim < 0.5 :
+        if sim < 0.5:
             continue
-        chem_sim_graph.add_unqualified_edge(pybel.dsl.Abundance(namespace='pubchem', name=pubchem_1), pybel.dsl.Abundance(namespace='pubchem', name=pubchem_2), 'similar_to')
+        chem_sim_graph.add_unqualified_edge(pybel.dsl.Abundance(namespace='pubchem', name=pubchem_1),
+                                            pybel.dsl.Abundance(namespace='pubchem', name=pubchem_2), 'association')
     return chem_sim_graph
