ed60088f89e609e1cc5f1ff8bb3c18515dd540d1
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2019-05-24 13:15:50 +0200

    add script for quick cluster run

---
+++ src/SE_KGE/cluster_weights.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pybel
+import networkx as nx
+import tqdm
+
+full_graph = pybel.from_pickle('/home/bio/groupshare/rana/full_graph.pickle')
+cluster_df = pd.read_csv('/home/bio/groupshare/rana/src/SE_KGE/resources/Clustered_chemicals.csv')
+
+clusters_dict = {i : cluster_df['PubchemID'].loc[cluster_df['Cluster'] == i].tolist()
+                for i in range(1,cluster_df.Cluster.nunique()+1)}
+subgraphs_dict = {}
+for cluster, chemicals in tqdm(clusters_dict.items()):
+    chemicals_subgraph = []
+    for chemical in chemicals:
+        chemical = pybel.dsl.Abundance(namespace='pubchem', name=str(chemical))
+        if chemical not in full_graph.nodes():
+            continue
+        for neighbor in full_graph.neighbors(chemical):
+            chemicals_subgraph.append(neighbor)
+    subgraphs_dict[cluster] = list(dict.fromkeys(chemicals_subgraph)) # to remove duplicates
+fullgraph_edges = len(full_graph.edges())
+cluster_weights = {}
+for cluster, nodes in tqdm(subgraphs_dict.items()):
+    subgraph = full_graph.subgraph(nodes)
+    edges = len(subgraph.edges())
+    cluster_weights[cluster] = edges/fullgraph_edges
+
+cluster_df['weight'] = None
+for cluster, weight in cluster_weights.items():
+    cluster_df.loc[cluster_df['Cluster'] == cluster, 'weight'] == weight
+cluster_df.to_csv('/home/bio/groupshare/rana/results/clusters_weights.csv')
