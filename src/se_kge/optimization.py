27d01e66ff30e137ba40515e7a39df1bcf31f347
aldisi <35561773+AldisiRana@users.noreply.github.com>
2019-07-11 09:50:01 +0200

    fix typos, add LINE optimization

--- src/se_kge/optimization.py
+++ src/se_kge/optimization.py
@@ -68,7 +68,7 @@ def node2vec_optimization(*, G, G_train, testing_pos_edges, train_graph_filename
         number_walks = trial.suggest_int('number_walks', 16, 128)
         window_size = trial.suggest_int('window_size', 1, 10)
         p = trial.suggest_uniform('p', 0, 4.0)
-        q = trial.suggest_uniform('p', 0, 4.0)
+        q = trial.suggest_uniform('q', 0, 4.0)
         model = embed_train.train_embed_node2vec(
             train_graph_filename=train_graph_filename,
             dimensions=dimensions,
@@ -101,8 +101,6 @@ def sdne_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, tr
         alpha = trial.suggest_uniform('alpha', 0, 0.4)
         beta = trial.suggest_int('beta', 0, 30)
         epochs = trial.suggest_int('epochs', 5, 30)
-        p = trial.suggest_uniform('p', 0, 4.0)
-        q = trial.suggest_uniform('p', 0, 4.0)
         model = embed_train.train_embed_sdne(
             train_graph_filename=train_graph_filename,
             alpha=alpha,
@@ -150,5 +148,33 @@ def grarep_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, 
     study = optuna.create_study(storage='sqlite:///grarep.db')
     study.set_user_attr('Author', 'Rana')
     study.set_user_attr('Method', 'GraRep')
+    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+    study.optimize(objective, n_trials=trial_number)
+
+def line_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, trial_number):
+    def objective(trial):
+        dimensions = trial.suggest_int('dimensions', 100, 300)
+        order = trial.suggest_int('order', 1, 3)
+        epochs = trial.suggest_int('epochs', 5, 30)
+        model = embed_train.train_embed_line(
+            train_graph_filename=train_graph_filename,
+            dimensions=dimensions,
+            order=order,
+            epochs=epochs)
+        embeddings = model.get_embeddings()
+        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(embeddings=embeddings,
+                                                                         original_graph=G,
+                                                                         train_graph=G_train,
+                                                                         test_pos_edges=testing_pos_edges, seed=0)
+        trial.set_user_attr('mcc', mcc)
+        trial.set_user_attr('auc_roc', auc_roc)
+        trial.set_user_attr('auc_pr', auc_pr)
+        trial.set_user_attr('accuracy', accuracy)
+        trial.set_user_attr('f1', f1)
+        return 1.0 - mcc
+
+    study = optuna.create_study(storage='sqlite:///sdne.db')
+    study.set_user_attr('Author', 'Rana')
+    study.set_user_attr('Method', 'SDNE')
     study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     study.optimize(objective, n_trials=trial_number)
