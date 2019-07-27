744347dc99fc802bdf5777bdfc4e3f5146e07735
aldisi <35561773+AldisiRana@users.noreply.github.com>
2019-07-27 23:59:47 +0200

    fix errors in code
    rename variables, add relations_list to prediction function

--- src/se_kge/find_relations.py
+++ src/se_kge/find_relations.py
@@ -12,17 +12,17 @@ def find_new_relations(*, entity, saved_model, node_mapping_file, embeddings_fil
     node_mapping = pd.read_csv(node_mapping_file, sep=',')
     graph = nx.read_edgelist(graph_edgelist)
     model = joblib.load(saved_model)
-    node_id = node_mapping.loc[node_mapping["NodeName"] == entity, "NodeID"].iloc[0]
-    node1 = embeddings[str(node_id)]
     if entity_type == 'chemical':
-        node_list = find_chemicals(entity=entity, embeddings=embeddings, node_mapping=node_mapping, graph=graph)
+        node_list, relations_list = find_chemicals(entity=entity, embeddings=embeddings, node_mapping=node_mapping, graph=graph)
     elif entity_type == 'phenotype':
-        node_list = find_phenotypes(entity=entity, embeddings=embeddings, node_mapping=node_mapping, graph=graph)
+        node_list, relations_list = find_phenotypes(entity=entity, embeddings=embeddings, node_mapping=node_mapping, graph=graph)
     elif entity_type == 'target':
-        node_list = find_targets(entity=entity, embeddings=embeddings, node_mapping=node_mapping, graph=graph)
+        node_list, relations_list = find_targets(entity=entity, embeddings=embeddings, node_mapping=node_mapping, graph=graph)
     else:
-        X=[]
+        relations_list=[]
         node_list = []
+        node_id = node_mapping.loc[node_mapping["NodeName"] == entity, "NodeID"].iloc[0]
+        node1 = embeddings[str(node_id)]
         for node, vector in embeddings.items():
             if node == entity:
                 continue
@@ -30,15 +30,17 @@ def find_new_relations(*, entity, saved_model, node_mapping_file, embeddings_fil
                 if graph.has_edge(entity, node) or graph.has_edge(node, entity):
                     continue
             node2 = np.array(embeddings[node])
-            x = node1 * node2
-            X.append(x.tolist())
+            relation = node1 * node2
+            relations_list.append(relation.tolist())
             node_list.append(node)
-    prediction_list = get_probabilities(node_list=node_list, model=model, k=k)
+    prediction_list = get_probabilities(node_list=node_list, relations_list=relations_list, model=model, k=k)
     return prediction_list
 
 def find_chemicals(*, entity, embeddings, graph=None, node_mapping):
-    X=[]
+    relations_list=[]
     node_list = []
+    node_id = node_mapping.loc[node_mapping["NodeName"] == entity, "NodeID"].iloc[0]
+    node1 = embeddings[str(node_id)]
     for node, vector in embeddings.items():
         if node == entity:
             continue
@@ -49,14 +51,16 @@ def find_chemicals(*, entity, embeddings, graph=None, node_mapping):
         if namespace != 'pubchem':
             continue
         node2 = np.array(embeddings[node])
-        x = node1 * node2
-        X.append(x.tolist())
+        relation = node1 * node2
+        relations_list.append(relation.tolist())
         node_list.append(node_mapping.loc[node_mapping["NodeID"] == int(node), "NodeName"].iloc[0])
-    return node_list
+    return node_list, relations_list
 
 def find_targets(*, entity, embeddings, graph=None, node_mapping):
-    X=[]
+    relations_list=[]
     node_list = []
+    node_id = node_mapping.loc[node_mapping["NodeName"] == entity, "NodeID"].iloc[0]
+    node1 = embeddings[str(node_id)]
     for node, vector in embeddings.items():
         if node == entity:
             continue
@@ -67,14 +71,16 @@ def find_targets(*, entity, embeddings, graph=None, node_mapping):
         if namespace != 'uniprot':
             continue
         node2 = np.array(embeddings[node])
-        x = node1 * node2
-        X.append(x.tolist())
+        relation = node1 * node2
+        relations_list.append(relation.tolist())
         node_list.append(node_mapping.loc[node_mapping["NodeID"] == int(node), "NodeName"].iloc[0])
-    return node_list
+    return node_list, relations_list
 
 def find_phenotypes(*, entity, embeddings, graph=None, node_mapping):
-    X=[]
+    relations_list=[]
     node_list = []
+    node_id = node_mapping.loc[node_mapping["NodeName"] == entity, "NodeID"].iloc[0]
+    node1 = embeddings[str(node_id)]
     for node, vector in embeddings.items():
         if node == entity:
             continue
@@ -85,14 +91,14 @@ def find_phenotypes(*, entity, embeddings, graph=None, node_mapping):
         if namespace != 'umls':
             continue
         node2 = np.array(embeddings[node])
-        x = node1 * node2
-        X.append(x.tolist())
+        relation = node1 * node2
+        relations_list.append(relation.tolist())
         node_list.append(node_mapping.loc[node_mapping["NodeID"] == int(node), "NodeName"].iloc[0])
-    return node_list
+    return node_list, relations_list
 
-def get_probabilities(*, node_list, model, k):
+def get_probabilities(*, node_list, relations_list, model, k):
     all_prob = {}
-    prob_list = model.predict_proba(X)[:,1]
+    prob_list = model.predict_proba(relations_list)[:,1]
     for i in range(len(node_list)):
         all_prob[node_list[i]] = prob_list[i]
     sorted_list = sorted(all_prob.items(), key=lambda kv: kv[1], reverse=True)
