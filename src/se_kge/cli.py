f177d422ca2f199527b5d67edc29d64da588e2e0
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-07-29 15:25:32 +0200

    Reorganize CLI
    Co-Authored-By: Rana Aldisi <aldisirana@users.noreply.github.com>

--- src/se_kge/cli.py
+++ src/se_kge/cli.py
@@ -2,115 +2,131 @@
 
 """Command line interface for ``se_kge``."""
 
-from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
-
 import random
 
+import click
 from bionev import pipeline
+
 from .optimization import (
     deepwalk_optimization, grarep_optimization, hope_optimization, line_optimization,
     node2vec_optimization, sdne_optimization,
 )
 
 
-def parse_args():
-    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,
-                            conflict_handler='resolve')
-    parser.add_argument('--input', required=True,
-                        help='Input graph file. Only accepted edgelist format.')
-    parser.add_argument('--training', default=None,
-                        help='training graph file. Only accepted edgelist format.')
-    parser.add_argument('--testing', default=None,
-                        help='testing graph file. Only accepted edgelist format.')
-    parser.add_argument('--method', required=True,
-                        help='The embedding learning method')
-    parser.add_argument('--trials', default=50, type=int,
-                        help='the number of trials done to optimize hyperparameters')
-    parser.add_argument('--dimensions', nargs='+', default=[100, 300], type=int,
-                        help='the range of dimensions to be optimized')
-    args = parser.parse_args()
-
-    return args
-
-
+@click.group()
 def main():
-    args = parse_args()
-    seed = random.randint(1, 10000000)
-    if args.training and args.testing is not None:
-        G, G_train, testing_pos_edges, train_graph_filename = pipeline.train_test_graph(
-            args.input,
-            args.training,
-            args.testing)
+    """Side Effects Knowledge Graph Embeddings."""
+
+
+@main.command()
+@click.option('--input-path', required=True, help='Input graph file. Only accepted edgelist format.')
+@click.option('--training', default=None, help='training graph file. Only accepted edgelist format.')
+@click.option('--testing', default=None, help='testing graph file. Only accepted edgelist format.')
+@click.option('--method', required=True, help='The embedding learning method')
+@click.option('--trials', default=50, type=int, help='the number of trials done to optimize hyperparameters')
+@click.option('--dimensions-low', type=int, help='the range of dimensions to be optimized')
+@click.option('--dimensions-high', type=int, help='the range of dimensions to be optimized')
+@click.option('--seed', type=int, default=random.randint(1, 10000000))
+@click.option('--storage', help="SQL connection string for study database. Example: sqlite:///optuna.db")
+def optimize(input_path, training, testing, method, trials, dimensions_low, dimensions_high, seed, storage):
+    """Run the optimization pipeline for a given method and graph."""
+    if training and testing is not None:
+        graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.train_test_graph(
+            input_path,
+            training,
+            testing,
+        )
     else:
-        G, G_train, testing_pos_edges, train_graph_filename = pipeline.split_train_test_graph(
-            args.input,
+        graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.split_train_test_graph(
+            input_path,
             seed
         )
-    if args.method == 'HOPE':
-        hope_optimization(
-            G=G,
-            G_train=G_train,
+    if method == 'HOPE':
+        study = hope_optimization(
+            graph=graph,
+            graph_train=graph_train,
             testing_pos_edges=testing_pos_edges,
             train_graph_filename=train_graph_filename,
-            trial_number=args.trials,
+            trial_number=trials,
             seed=seed,
-            dimensions_range=args.dimensions
+            dimensions_range=(dimensions_low, dimensions_high),
+            storage=storage,
         )
 
-    elif args.method == 'DeepWalk':
-        deepwalk_optimization(
-            G=G,
-            G_train=G_train,
+    elif method == 'DeepWalk':
+        study = deepwalk_optimization(
+            graph=graph,
+            graph_train=graph_train,
             testing_pos_edges=testing_pos_edges,
             train_graph_filename=train_graph_filename,
-            trial_number=args.trials,
+            trial_number=trials,
             seed=seed,
-            dimensions_range=args.dimensions
-
+            dimensions_range=(dimensions_low, dimensions_high),
+            storage=storage,
         )
 
-    elif args.method == 'node2vec':
-        node2vec_optimization(
-            G=G,
-            G_train=G_train,
+    elif method == 'node2vec':
+        study = node2vec_optimization(
+            graph=graph,
+            graph_train=graph_train,
             testing_pos_edges=testing_pos_edges,
             train_graph_filename=train_graph_filename,
-            trial_number=args.trials,
+            trial_number=trials,
             seed=seed,
-            dimensions_range=args.dimensions
+            dimensions_range=(dimensions_low, dimensions_high),
+            storage=storage,
         )
 
-    elif args.method == 'GraRep':
-        grarep_optimization(
-            G=G,
-            G_train=G_train,
+    elif method == 'GraRep':
+        study = grarep_optimization(
+            graph=graph,
+            graph_train=graph_train,
             testing_pos_edges=testing_pos_edges,
             train_graph_filename=train_graph_filename,
-            trial_number=args.trials,
+            trial_number=trials,
             seed=seed,
-            dimensions_range=args.dimensions
+            dimensions_range=(dimensions_low, dimensions_high),
+            storage=storage,
         )
 
-    elif args.method == 'SDNE':
-        sdne_optimization(
-            G=G,
-            G_train=G_train,
+    elif method == 'SDNE':
+        study = sdne_optimization(
+            graph=graph,
+            graph_train=graph_train,
             testing_pos_edges=testing_pos_edges,
             train_graph_filename=train_graph_filename,
-            trial_number=args.trials,
-            seed=seed
-        )
-
-    elif args.method == 'LINE':
-        line_optimization(
-            G=G,
-            G_train=G_train,
-            testing_pos_edges=testing_pos_edges,
-            train_graph_filename=train_graph_filename,
-            trial_number=args.trials,
+            trial_number=trials,
             seed=seed,
-            dimensions_range=args.dimensions
+            storage=storage,
         )
+
+    elif method == 'LINE':
+        study = line_optimization(
+            graph=graph,
+            graph_train=graph_train,
+            testing_pos_edges=testing_pos_edges,
+            train_graph_filename=train_graph_filename,
+            trial_number=trials,
+            seed=seed,
+            dimensions_range=(dimensions_low, dimensions_high),
+            storage=storage,
+        )
+
+    else:
+        raise ValueError(f'Invalid method: {method}')
+
+    # TODO output something about the study
+
+
+@main.command()
+def train():
+    """Train my model"""
+
+
+@main.command()
+def web():
+    """Run the se_kge RESTful API."""
+    # TODO
 
 
 if __name__ == "__main__":
--- src/se_kge/optimization.py
+++ src/se_kge/optimization.py
@@ -1,42 +1,89 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+from getpass import getuser
+from typing import Union
 
 import optuna
 from bionev import embed_train, pipeline
+from bionev.OpenNE.line import LINE
+from optuna import Study
+from optuna.storages import BaseStorage
 
 
-def hope_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, trial_number, seed, dimensions_range):
+def run_study(
+        objective,
+        n_trials: int,
+        storage: Union[None, str, BaseStorage] = None,
+) -> Study:
+    study = optuna.create_study(storage=storage)
+    study.set_user_attr('Author', getuser())
+    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+    study.optimize(objective, n_trials=n_trials)
+    return study
+
+
+def predict_and_evaluate(model, graph, graph_train, testing_pos_edges, seed, trial):
+    if isinstance(model, LINE):
+        embeddings = model.get_embeddings_train()
+    else:
+        embeddings = model.get_embeddings()
+    auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
+        embeddings=embeddings,
+        original_graph=graph,
+        train_graph=graph_train,
+        test_pos_edges=testing_pos_edges,
+        seed=seed,
+    )
+    trial.set_user_attr('mcc', mcc)
+    trial.set_user_attr('auc_roc', auc_roc)
+    trial.set_user_attr('auc_pr', auc_pr)
+    trial.set_user_attr('accuracy', accuracy)
+    trial.set_user_attr('f1', f1)
+
+    return 1.0 - mcc
+
+
+def hope_optimization(
+        *,
+        graph,
+        graph_train,
+        testing_pos_edges,
+        train_graph_filename,
+        trial_number,
+        seed,
+        dimensions_range,
+        storage=None,
+):
     def objective(trial):
+        trial.set_user_attr('method', 'hope')
+        trial.set_user_attr('seed', seed)
+
         dimensions = trial.suggest_int('dimensions', dimensions_range[0], dimensions_range[1])
         model = embed_train.train_embed_hope(
             train_graph_filename=train_graph_filename,
-            dimensions=dimensions)
-        embeddings = model.get_embeddings()
-        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
-            embeddings=embeddings,
-            original_graph=G,
-            train_graph=G_train,
-            test_pos_edges=testing_pos_edges,
-            seed=seed,
-            save_model=None)
-        trial.set_user_attr('seed', seed)
-        trial.set_user_attr('mcc', mcc)
-        trial.set_user_attr('auc_roc', auc_roc)
-        trial.set_user_attr('auc_pr', auc_pr)
-        trial.set_user_attr('accuracy', accuracy)
-        trial.set_user_attr('f1', f1)
-        return 1.0 - mcc
+            dimensions=dimensions,
+        )
+        return predict_and_evaluate(model, graph, graph_train, testing_pos_edges, seed, trial)
 
-    study = optuna.create_study(storage='sqlite:///hope.db')
-    study.set_user_attr('Author', 'Rana')
-    study.set_user_attr('Method', 'HOPE')
-    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    study.optimize(objective, n_trials=trial_number)
+    return run_study(objective, trial_number, storage)
 
 
-def deepwalk_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, trial_number, seed, dimensions_range):
+def deepwalk_optimization(
+        *,
+        graph,
+        graph_train,
+        testing_pos_edges,
+        train_graph_filename,
+        trial_number,
+        seed,
+        dimensions_range,
+        storage=None,
+):
     def objective(trial):
+        trial.set_user_attr('method', 'deepwalk')
+        trial.set_user_attr('seed', seed)
+
         dimensions = trial.suggest_int('dimensions', dimensions_range[0], dimensions_range[1])
         walk_length = trial.suggest_int('walk_length', 16, 128)
         number_walks = trial.suggest_int('number_walks', 16, 128)
@@ -46,32 +93,28 @@ def deepwalk_optimization(*, G, G_train, testing_pos_edges, train_graph_filename
             dimensions=dimensions,
             walk_length=walk_length,
             number_walks=number_walks,
-            window_size=window_size)
-        embeddings = model.get_embeddings()
-        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
-            embeddings=embeddings,
-            original_graph=G,
-            train_graph=G_train,
-            test_pos_edges=testing_pos_edges,
-            seed=seed,
-            save_model=None)
-        trial.set_user_attr('seed', seed)
-        trial.set_user_attr('mcc', mcc)
-        trial.set_user_attr('auc_roc', auc_roc)
-        trial.set_user_attr('auc_pr', auc_pr)
-        trial.set_user_attr('accuracy', accuracy)
-        trial.set_user_attr('f1', f1)
-        return 1.0 - mcc
+            window_size=window_size,
+        )
+        return predict_and_evaluate(model, graph, graph_train, testing_pos_edges, seed, trial)
 
-    study = optuna.create_study(storage='sqlite:///deepwalk.db')
-    study.set_user_attr('Author', 'Rana')
-    study.set_user_attr('Method', 'DeepWalk')
-    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    study.optimize(objective, n_trials=trial_number)
+    return run_study(objective, trial_number, storage)
 
 
-def node2vec_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, trial_number, seed, dimensions_range):
+def node2vec_optimization(
+        *,
+        graph,
+        graph_train,
+        testing_pos_edges,
+        train_graph_filename,
+        trial_number,
+        seed,
+        dimensions_range,
+        storage=None,
+):
     def objective(trial):
+        trial.set_user_attr('method', 'node2vec')
+        trial.set_user_attr('seed', seed)
+
         dimensions = trial.suggest_int('dimensions', dimensions_range[0], dimensions_range[1])
         walk_length = trial.suggest_int('walk_length', 16, 128)
         number_walks = trial.suggest_int('number_walks', 16, 128)
@@ -86,31 +129,25 @@ def node2vec_optimization(*, G, G_train, testing_pos_edges, train_graph_filename
             window_size=window_size,
             p=p,
             q=q)
-        embeddings = model.get_embeddings()
-        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
-            embeddings=embeddings,
-            original_graph=G,
-            train_graph=G_train,
-            test_pos_edges=testing_pos_edges,
-            seed=seed,
-            save_model=None)
-        trial.set_user_attr('seed', seed)
-        trial.set_user_attr('mcc', mcc)
-        trial.set_user_attr('auc_roc', auc_roc)
-        trial.set_user_attr('auc_pr', auc_pr)
-        trial.set_user_attr('accuracy', accuracy)
-        trial.set_user_attr('f1', f1)
-        return 1.0 - mcc
+        return predict_and_evaluate(model, graph, graph_train, testing_pos_edges, seed, trial)
 
-    study = optuna.create_study(storage='sqlite:///node2vec.db')
-    study.set_user_attr('Author', 'Rana')
-    study.set_user_attr('Method', 'node2vec')
-    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    study.optimize(objective, n_trials=trial_number)
+    return run_study(objective, trial_number, storage)
 
 
-def sdne_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, trial_number, seed):
+def sdne_optimization(
+        *,
+        graph,
+        graph_train,
+        testing_pos_edges,
+        train_graph_filename,
+        trial_number,
+        seed,
+        storage=None,
+):
     def objective(trial):
+        trial.set_user_attr('method', 'sdne')
+        trial.set_user_attr('seed', seed)
+
         alpha = trial.suggest_uniform('alpha', 0, 0.4)
         beta = trial.suggest_int('beta', 0, 30)
         epochs = trial.suggest_int('epochs', 5, 30)
@@ -118,60 +155,55 @@ def sdne_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, tr
             train_graph_filename=train_graph_filename,
             alpha=alpha,
             beta=beta,
-            epochs=epochs)
-        embeddings = model.get_embeddings()
-        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(embeddings=embeddings,
-                                                                         original_graph=G,
-                                                                         train_graph=G_train,
-                                                                         test_pos_edges=testing_pos_edges, seed=seed)
-        trial.set_user_attr('seed', seed)
-        trial.set_user_attr('mcc', mcc)
-        trial.set_user_attr('auc_roc', auc_roc)
-        trial.set_user_attr('auc_pr', auc_pr)
-        trial.set_user_attr('accuracy', accuracy)
-        trial.set_user_attr('f1', f1)
-        return 1.0 - mcc
+            epochs=epochs,
+        )
+        return predict_and_evaluate(model, graph, graph_train, testing_pos_edges, seed, trial)
 
-    study = optuna.create_study(storage='sqlite:///sdne.db')
-    study.set_user_attr('Author', 'Rana')
-    study.set_user_attr('Method', 'SDNE')
-    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    study.optimize(objective, n_trials=trial_number)
+    return run_study(objective, trial_number, storage)
 
 
-def grarep_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, trial_number, seed, dimensions_range):
+def grarep_optimization(
+        *,
+        graph,
+        graph_train,
+        testing_pos_edges,
+        train_graph_filename,
+        trial_number,
+        seed,
+        dimensions_range,
+        storage=None,
+):
     def objective(trial):
+        trial.set_user_attr('method', 'grarep')
+        trial.set_user_attr('seed', seed)
+
         dimensions = trial.suggest_int('dimensions', dimensions_range[0], dimensions_range[1])
         kstep = trial.suggest_int('kstep', 1, 10)
         model = embed_train.train_embed_grarep(
             train_graph_filename=train_graph_filename,
             dimensions=dimensions,
-            kstep=kstep)
-        embeddings = model.get_embeddings()
-        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
-            embeddings=embeddings,
-            original_graph=G,
-            train_graph=G_train,
-            test_pos_edges=testing_pos_edges,
-            seed=seed,
-            save_model=None)
-        trial.set_user_attr('seed', seed)
-        trial.set_user_attr('mcc', mcc)
-        trial.set_user_attr('auc_roc', auc_roc)
-        trial.set_user_attr('auc_pr', auc_pr)
-        trial.set_user_attr('accuracy', accuracy)
-        trial.set_user_attr('f1', f1)
-        return 1.0 - mcc
+            kstep=kstep,
+        )
+        return predict_and_evaluate(model, graph, graph_train, testing_pos_edges, seed, trial)
 
-    study = optuna.create_study(storage='sqlite:///grarep.db')
-    study.set_user_attr('Author', 'Rana')
-    study.set_user_attr('Method', 'GraRep')
-    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    study.optimize(objective, n_trials=trial_number)
+    return run_study(objective, trial_number, storage)
 
 
-def line_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, trial_number, seed, dimensions_range):
+def line_optimization(
+        *,
+        graph,
+        graph_train,
+        testing_pos_edges,
+        train_graph_filename,
+        trial_number,
+        seed,
+        dimensions_range,
+        storage=None,
+):
     def objective(trial):
+        trial.set_user_attr('method', 'line')
+        trial.set_user_attr('seed', seed)
+
         dimensions = trial.suggest_int('dimensions', dimensions_range[0], dimensions_range[1])
         order = trial.suggest_int('order', 1, 3)
         epochs = trial.suggest_int('epochs', 5, 30)
@@ -179,25 +211,8 @@ def line_optimization(*, G, G_train, testing_pos_edges, train_graph_filename, tr
             train_graph_filename=train_graph_filename,
             dimensions=dimensions,
             order=order,
-            epochs=epochs)
-        embeddings = model.get_embeddings_train()
-        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
-            embeddings=embeddings,
-            original_graph=G,
-            train_graph=G_train,
-            test_pos_edges=testing_pos_edges,
-            seed=seed,
-            save_model=None)
-        trial.set_user_attr('seed', seed)
-        trial.set_user_attr('mcc', mcc)
-        trial.set_user_attr('auc_roc', auc_roc)
-        trial.set_user_attr('auc_pr', auc_pr)
-        trial.set_user_attr('accuracy', accuracy)
-        trial.set_user_attr('f1', f1)
-        return 1.0 - mcc
+            epochs=epochs,
+        )
+        return predict_and_evaluate(model, graph, graph_train, testing_pos_edges, seed, trial)
 
-    study = optuna.create_study(storage='sqlite:///line.db')
-    study.set_user_attr('Author', 'Rana')
-    study.set_user_attr('Method', 'LINE')
-    study.set_user_attr('Date', datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    study.optimize(objective, n_trials=trial_number)
+    return run_study(objective, trial_number, storage)
