c75803204444000636bfe8ed497f6935dae79a04
aldisi <35561773+AldisiRana@users.noreply.github.com>
2019-08-10 23:23:14 +0200

    refactor cli
    closes #14

--- src/se_kge/cli.py
+++ src/se_kge/cli.py
@@ -2,26 +2,28 @@
 
 """Command line interface for ``se_kge``."""
 
-import datetime
-import getpass
 import json
 import logging
 import random
 import sys
 
 import click
-import networkx as nx
-from bionev import pipeline
-from bionev.embed_train import embedding_training
 from se_kge.graph_preprocessing import get_mapped_graph
 
 from .constants import DEFAULT_GRAPH_PATH
 from .find_relations import RESULTS_TYPE_TO_NAMESPACE
-from .optimization import (
-    deepwalk_optimization, grarep_optimization, hope_optimization, line_optimization,
-    node2vec_optimization, sdne_optimization,
-)
-from .utils import study_to_json
+
+from .utils import do_evaluation, do_optimization, train_model
+
+
+INPUT_PATH = click.option('--input-path', default=DEFAULT_GRAPH_PATH,
+                          help='Input graph file. Only accepted edgelist format.')
+TRAINING_PATH = click.option('--training-path', help='training graph file. Only accepted edgelist format.')
+TESTING_PATH = click.option('--testing-path', help='testing graph file. Only accepted edgelist format.')
+METHOD = click.option('--method', required=True,
+                      type=click.Choice(['node2vec', 'DeepWalk', 'HOPE', 'GraRep', 'LINE', 'SDNE']),
+                      help='The NRL method to train the model')
+SEED = click.option('--seed', type=int, default=random.randint(1, 10000000))
 
 
 @click.group()
@@ -30,23 +32,20 @@ def main():
 
 
 @main.command()
-@click.option('--input-path', default=DEFAULT_GRAPH_PATH,
-              help='Input graph file. Only accepted edgelist format.')
-@click.option('--training', help='training graph file. Only accepted edgelist format.')
-@click.option('--testing', help='testing graph file. Only accepted edgelist format.')
-@click.option('--method', required=True,
-              type=click.Choice(['node2vec', 'DeepWalk', 'HOPE', 'GraRep', 'LINE', 'SDNE']),
-              help='The NRL method to train the model')
+@INPUT_PATH
+@TRAINING_PATH
+@TESTING_PATH
+@METHOD
+@SEED
 @click.option('--trials', default=50, type=int, help='the number of trials done to optimize hyperparameters')
 @click.option('--dimensions-range', default=(100, 300), type=(int, int), help='the range of dimensions to be optimized')
-@click.option('--seed', type=int, default=random.randint(1, 10000000))
 @click.option('--storage', help="SQL connection string for study database. Example: sqlite:///optuna.db")
 @click.option('--name', help="Name for the study")
 @click.option('-o', '--output', type=click.File('w'), help="Output study summary", default=sys.stdout)
 def optimize(
         input_path,
-        training,
-        testing,
+        training_path,
+        testing_path,
         method,
         trials,
         dimensions_range,
@@ -56,112 +55,32 @@ def optimize(
         output,
 ):
     """Run the optimization pipeline for a given method and graph."""
-    if training and testing is not None:
-        graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.train_test_graph(
-            input_path,
-            training,
-            testing,
-        )
-    else:
-        graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.split_train_test_graph(
-            input_path,
-            seed
-        )
-    if method == 'HOPE':
-        study = hope_optimization(
-            graph=graph,
-            graph_train=graph_train,
-            testing_pos_edges=testing_pos_edges,
-            train_graph_filename=train_graph_filename,
-            trial_number=trials,
-            seed=seed,
-            dimensions_range=dimensions_range,
-            storage=storage,
-            study_name=name,
-        )
-
-    elif method == 'DeepWalk':
-        study = deepwalk_optimization(
-            graph=graph,
-            graph_train=graph_train,
-            testing_pos_edges=testing_pos_edges,
-            train_graph_filename=train_graph_filename,
-            trial_number=trials,
-            seed=seed,
-            dimensions_range=dimensions_range,
-            storage=storage,
-            study_name=name,
-        )
-
-    elif method == 'node2vec':
-        study = node2vec_optimization(
-            graph=graph,
-            graph_train=graph_train,
-            testing_pos_edges=testing_pos_edges,
-            train_graph_filename=train_graph_filename,
-            trial_number=trials,
-            seed=seed,
-            dimensions_range=dimensions_range,
-            storage=storage,
-            study_name=name,
-        )
-
-    elif method == 'GraRep':
-        study = grarep_optimization(
-            graph=graph,
-            graph_train=graph_train,
-            testing_pos_edges=testing_pos_edges,
-            train_graph_filename=train_graph_filename,
-            trial_number=trials,
-            seed=seed,
-            dimensions_range=dimensions_range,
-            storage=storage,
-            study_name=name,
-        )
-
-    elif method == 'SDNE':
-        study = sdne_optimization(
-            graph=graph,
-            graph_train=graph_train,
-            testing_pos_edges=testing_pos_edges,
-            train_graph_filename=train_graph_filename,
-            trial_number=trials,
-            seed=seed,
-            storage=storage,
-            study_name=name,
-        )
-
-    else:
-        study = line_optimization(
-            graph=graph,
-            graph_train=graph_train,
-            testing_pos_edges=testing_pos_edges,
-            train_graph_filename=train_graph_filename,
-            trial_number=trials,
-            seed=seed,
-            dimensions_range=dimensions_range,
-            storage=storage,
-            study_name=name,
-        )
-
-    study_json = study_to_json(study)
-    json.dump(study_json, output, indent=2, sort_keys=True)
+    do_optimization(
+        input_path=input_path,
+        training_path=training_path,
+        testing_path=testing_path,
+        method=method,
+        trials=trials,
+        storage=storage,
+        dimensions_range=dimensions_range,
+        name=name,
+        output=output,
+        seed=seed,
+    )
 
 
 @main.command()
-@click.option('--input-path', default=DEFAULT_GRAPH_PATH, help='Input graph file. Only accepted edgelist format.')
-@click.option('--training', help='training graph file. Only accepted edgelist format.')
-@click.option('--testing', help='testing graph file. Only accepted edgelist format.')
+@INPUT_PATH
+@TRAINING_PATH
+@TESTING_PATH
+@SEED
+@METHOD
 @click.option('--evaluation', is_flag=True, help='If true, a testing set will be used to evaluate model.')
 @click.option('--evaluation-file', type=click.File('w'), default=sys.stdout,
               help='The path to save evaluation results.')
 @click.option('--embeddings-path', help='The path to save the embeddings file')
 @click.option('--model-path',
               help='The path to save the prediction model')
-@click.option('--seed', type=int, default=random.randint(1, 10000000))
-@click.option('--method', required=True,
-              type=click.Choice(['node2vec', 'DeepWalk', 'HOPE', 'GraRep', 'LINE', 'SDNE']),
-              help='The NRL method to train the model')
 @click.option('--dimensions', type=int, default=200,
               help='The dimensions of embeddings.')
 @click.option('--number-walks', type=int, default=8,
@@ -186,8 +105,8 @@ def optimize(
               help='The order parameter for LINE')
 def train(
         input_path,
-        training,
-        testing,
+        training_path,
+        testing_path,
         evaluation,
         evaluation_file,
         embeddings_path,
@@ -208,19 +127,10 @@ def train(
 ):
     """Train my model."""
     if evaluation:
-        if training and testing is not None:
-            graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.train_test_graph(
-                input_path,
-                training,
-                testing,
-            )
-        else:
-            graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.split_train_test_graph(
-                input_path,
-                seed
-            )
-        model = embedding_training(
-            train_graph_filename=train_graph_filename,
+        results = do_evaluation(
+            input_path=input_path,
+            training_path=training_path,
+            testing_path=testing_path,
             method=method,
             dimensions=dimensions,
             number_walks=number_walks,
@@ -234,36 +144,15 @@ def train(
             kstep=kstep,
             order=order,
             seed=seed,
+            embeddings_path=embeddings_path,
+            model_path=model_path,
+            evaluation_file=evaluation_file
         )
-        model.save_embeddings(embeddings_path)
-        auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
-            embeddings=model.get_embeddings(),
-            original_graph=graph,
-            train_graph=graph_train,
-            test_pos_edges=testing_pos_edges,
-            seed=seed,
-            save_model=model_path
-        )
-        _results = dict(
-            input=input_path,
-            method=method,
-            dimension=dimensions,
-            user=getpass.getuser(),
-            date=datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S'),
-            seed=seed,
-        )
-        _results['results'] = dict(
-            auc_roc=auc_roc,
-            auc_pr=auc_pr,
-            accuracy=accuracy,
-            f1=f1,
-            mcc=mcc,
-        )
-        json.dump(_results, evaluation_file, sort_keys=True, indent=2)
-
+        click.echo('Training is finished.')
+        click.echo(results)
     else:
-        model = embedding_training(
-            train_graph_filename=input_path,
+        train_model(
+            input_path=input_path,
             method=method,
             dimensions=dimensions,
             number_walks=number_walks,
@@ -277,14 +166,8 @@ def train(
             kstep=kstep,
             order=order,
             seed=seed,
-        )
-        model.save_embeddings(embeddings_path)
-        original_graph = nx.read_edgelist(input_path)
-        pipeline.create_prediction_model(
-            embeddings=model.get_embeddings(),
-            original_graph=original_graph,
-            seed=seed,
-            save_model=model_path
+            model_path=model_path,
+            embeddings_path=embeddings_path,
         )
         click.echo('Training is finished.')
 
--- src/se_kge/utils.py
+++ src/se_kge/utils.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """Utilities for ``se_kge``."""
+import datetime
+import getpass
+import json
 from typing import Any, Mapping
 
+import networkx as nx
 import optuna
+from bionev import pipeline
+from bionev.embed_train import embedding_training
+
+from .optimization import (
+    deepwalk_optimization, grarep_optimization, hope_optimization, line_optimization,
+    node2vec_optimization, sdne_optimization,
+)
 
 
 def study_to_json(study: optuna.Study) -> Mapping[str, Any]:
@@ -25,3 +36,235 @@ def study_to_json(study: optuna.Study) -> Mapping[str, Any]:
             'value': study.best_value,
         },
     }
+
+
+def create_graphs(*, input_path, training_path, testing_path, seed):
+    if training_path and testing_path is not None:
+        graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.train_test_graph(
+            input_path,
+            training_path,
+            testing_path,
+        )
+    else:
+        graph, graph_train, testing_pos_edges, train_graph_filename = pipeline.split_train_test_graph(
+            input_path,
+            seed
+        )
+    return graph, graph_train, testing_pos_edges, train_graph_filename
+
+
+def do_evaluation(
+        *,
+        input_path,
+        training_path,
+        testing_path,
+        method,
+        dimensions,
+        number_walks,
+        walk_length,
+        window_size,
+        p,
+        q,
+        alpha,
+        beta,
+        epochs,
+        kstep,
+        order,
+        seed,
+        embeddings_path,
+        model_path,
+        evaluation_file
+):
+    graph, graph_train, testing_pos_edges, train_graph_filename = create_graphs(
+        input_path=input_path,
+        training_path=training_path,
+        testing_path=testing_path,
+        seed=seed,
+    )
+    model = embedding_training(
+        train_graph_filename=train_graph_filename,
+        method=method,
+        dimensions=dimensions,
+        number_walks=number_walks,
+        walk_length=walk_length,
+        window_size=window_size,
+        p=p,
+        q=q,
+        alpha=alpha,
+        beta=beta,
+        epochs=epochs,
+        kstep=kstep,
+        order=order,
+        seed=seed,
+    )
+    model.save_embeddings(embeddings_path)
+    auc_roc, auc_pr, accuracy, f1, mcc = pipeline.do_link_prediction(
+        embeddings=model.get_embeddings(),
+        original_graph=graph,
+        train_graph=graph_train,
+        test_pos_edges=testing_pos_edges,
+        seed=seed,
+        save_model=model_path
+    )
+    _results = dict(
+        input=input_path,
+        method=method,
+        dimension=dimensions,
+        user=getpass.getuser(),
+        date=datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S'),
+        seed=seed,
+    )
+    _results['results'] = dict(
+        auc_roc=auc_roc,
+        auc_pr=auc_pr,
+        accuracy=accuracy,
+        f1=f1,
+        mcc=mcc,
+    )
+    json.dump(_results, evaluation_file, sort_keys=True, indent=2)
+    return _results
+
+
+def do_optimization(
+        *,
+        method,
+        input_path,
+        training_path,
+        testing_path,
+        trials,
+        seed,
+        dimensions_range,
+        storage,
+        name,
+        output,
+):
+    graph, graph_train, testing_pos_edges, train_graph_filename = create_graphs(
+        input_path=input_path,
+        training_path=training_path,
+        testing_path=testing_path,
+        seed=seed,
+    )
+    if method == 'HOPE':
+        study = hope_optimization(
+            graph=graph,
+            graph_train=graph_train,
+            testing_pos_edges=testing_pos_edges,
+            train_graph_filename=train_graph_filename,
+            trial_number=trials,
+            seed=seed,
+            dimensions_range=dimensions_range,
+            storage=storage,
+            study_name=name,
+        )
+
+    elif method == 'DeepWalk':
+        study = deepwalk_optimization(
+            graph=graph,
+            graph_train=graph_train,
+            testing_pos_edges=testing_pos_edges,
+            train_graph_filename=train_graph_filename,
+            trial_number=trials,
+            seed=seed,
+            dimensions_range=dimensions_range,
+            storage=storage,
+            study_name=name,
+        )
+
+    elif method == 'node2vec':
+        study = node2vec_optimization(
+            graph=graph,
+            graph_train=graph_train,
+            testing_pos_edges=testing_pos_edges,
+            train_graph_filename=train_graph_filename,
+            trial_number=trials,
+            seed=seed,
+            dimensions_range=dimensions_range,
+            storage=storage,
+            study_name=name,
+        )
+
+    elif method == 'GraRep':
+        study = grarep_optimization(
+            graph=graph,
+            graph_train=graph_train,
+            testing_pos_edges=testing_pos_edges,
+            train_graph_filename=train_graph_filename,
+            trial_number=trials,
+            seed=seed,
+            dimensions_range=dimensions_range,
+            storage=storage,
+            study_name=name,
+        )
+
+    elif method == 'SDNE':
+        study = sdne_optimization(
+            graph=graph,
+            graph_train=graph_train,
+            testing_pos_edges=testing_pos_edges,
+            train_graph_filename=train_graph_filename,
+            trial_number=trials,
+            seed=seed,
+            storage=storage,
+            study_name=name,
+        )
+
+    else:
+        study = line_optimization(
+            graph=graph,
+            graph_train=graph_train,
+            testing_pos_edges=testing_pos_edges,
+            train_graph_filename=train_graph_filename,
+            trial_number=trials,
+            seed=seed,
+            dimensions_range=dimensions_range,
+            storage=storage,
+            study_name=name,
+        )
+
+    study_json = study_to_json(study)
+    json.dump(study_json, output, indent=2, sort_keys=True)
+
+
+def train_model(
+        *,
+        input_path,
+        method,
+        dimensions,
+        number_walks,
+        walk_length,
+        window_size,
+        p,
+        q,
+        alpha,
+        beta,
+        epochs,
+        kstep,
+        order,
+        seed,
+        model_path,
+        embeddings_path,
+):
+    model = embedding_training(
+        train_graph_filename=input_path,
+        method=method,
+        dimensions=dimensions,
+        number_walks=number_walks,
+        walk_length=walk_length,
+        window_size=window_size,
+        p=p,
+        q=q,
+        alpha=alpha,
+        beta=beta,
+        epochs=epochs,
+        kstep=kstep,
+        order=order,
+        seed=seed,
+    )
+    model.save_embeddings(embeddings_path)
+    original_graph = nx.read_edgelist(input_path)
+    pipeline.create_prediction_model(
+        embeddings=model.get_embeddings(),
+        original_graph=original_graph,
+        seed=seed,
+        save_model=model_path
+    )
