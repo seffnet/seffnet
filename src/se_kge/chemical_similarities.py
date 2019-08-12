b7b3df82901c59d234ac1225ececc2a0e201bf33
aldisi <35561773+AldisiRana@users.noreply.github.com>
2019-08-12 14:31:23 +0200

    add clustering function to python script and update clustering df

--- src/se_kge/chemical_similarities.py
+++ src/se_kge/chemical_similarities.py
@@ -9,20 +9,23 @@ Note: to run these the similarity function you need to have rdkit package
 
 import itertools as itt
 import os
-from collections import Iterable, Mapping
-from typing import Any
 
+import networkx as nx
 import pandas as pd
 import pybel
 from rdkit import Chem, DataStructs
 from rdkit.Chem import MACCSkeys
+from rdkit.ML.Cluster import Butina
 from tqdm import tqdm
 
-from .constants import PUBCHEM_NAMESPACE, RESOURCES
+from .constants import (
+    PUBCHEM_NAMESPACE, DEFAULT_CHEMICALS_MAPPING_PATH, DEFAULT_FULLGRAPH_WITHOUT_CHEMSIM_PICKLE, DEFAULT_GRAPH_PATH,
+    DEFAULT_FULLGRAPH_PICKLE, DEFAULT_CLUSTERED_CHEMICALS,
+    DEFAULT_CHEMSIM_PICKLE)
 from .get_url_requests import cid_to_smiles
 
 
-def get_smiles(pubchem_ids: Iterable[str]) -> Mapping[str, str]:
+def get_smiles(pubchem_ids):
     """
     Get SMILES of a list of chemicals.
 
@@ -40,7 +43,7 @@ def get_smiles(pubchem_ids: Iterable[str]) -> Mapping[str, str]:
     return pubchem_id_to_smiles
 
 
-def get_similarity(pubchem_id_to_smiles: Mapping[str, str]) -> Mapping[str, float]:
+def get_similarity(pubchem_id_to_smiles):
     """
     Get the similarities between all pair combinations of chemicals in the list.
 
@@ -56,7 +59,7 @@ def get_similarity(pubchem_id_to_smiles: Mapping[str, str]) -> Mapping[str, floa
     return chem_sim
 
 
-def get_fingerprints(pubchem_id_to_smiles: Mapping[str, str]) -> Mapping[str, Any]:
+def get_fingerprints(pubchem_id_to_smiles):
     """
     Create a dictionary containing the fingerprints for every chemical.
 
@@ -72,20 +75,18 @@ def get_fingerprints(pubchem_id_to_smiles: Mapping[str, str]) -> Mapping[str, An
     return pubchem_id_to_fingerprint
 
 
-def create_similarity_graph(
-        pubchem_ids: Iterable[str],
-        mapping_file=os.path.abspath(os.path.join(
-            RESOURCES,
-            "mapping",
-            "drugbank_pubchem_mapping.tsv")
-        ),
+def get_similarity_graph(
+        *,
+        fullgraph=DEFAULT_FULLGRAPH_WITHOUT_CHEMSIM_PICKLE,
+        rebuild: bool = False,
+        mapping_file=DEFAULT_CHEMICALS_MAPPING_PATH,
         similarity=0.5,
         name='',
         version='1.1.0',
         authors='',
         contact='',
         description='',
-) -> pybel.BELGraph:
+):
     """
     Create a BELGraph with chemicals as nodes, and similarity as edges.
 
@@ -93,6 +94,15 @@ def create_similarity_graph(
     :param similarity: the percent in which the chemicals are similar
     :param mapping_file: an existing dataframe with pubchemIDs and Smiles
     """
+    if not rebuild and os.path.exists(DEFAULT_GRAPH_PATH):
+        return nx.read_edgelist(DEFAULT_GRAPH_PATH)
+    fullgraph_without_chemsim = pybel.from_pickle(fullgraph)
+    pubchem_ids = []
+    for node in fullgraph_without_chemsim.nodes():
+        if node.namespace != 'pubchem.compound':
+            continue
+        pubchem_ids.append(node.identifier)
+
     if os.path.exists(mapping_file):
         chemicals_mapping = pd.read_csv(
             mapping_file,
@@ -112,13 +122,90 @@ def create_similarity_graph(
 
     similarities = get_similarity(pubchem_id_to_smiles)
 
-    bel_graph = pybel.BELGraph(name, version, description, authors, contact)
+    chemsim_graph = pybel.BELGraph(name, version, description, authors, contact)
     for (source_pubchem_id, target_pubchem_id), sim in tqdm(similarities.items(), desc='Creating BELGraph'):
         if sim < similarity:
             continue
-        bel_graph.add_unqualified_edge(
+        chemsim_graph.add_unqualified_edge(
             pybel.dsl.Abundance(namespace=PUBCHEM_NAMESPACE, identifier=source_pubchem_id),
             pybel.dsl.Abundance(namespace=PUBCHEM_NAMESPACE, identifier=target_pubchem_id),
             'association',
         )
-    return bel_graph
+    fullgraph_without_chemsim = pybel.from_pickle(DEFAULT_FULLGRAPH_WITHOUT_CHEMSIM_PICKLE)
+    fullgraph_with_chemsim = fullgraph_without_chemsim + chemsim_graph
+    relabel_graph = {}
+    i = 1
+    for node in fullgraph_with_chemsim.nodes():
+        relabel_graph[node] = i
+        i += 1
+    fullgraph_with_chemsim_relabeled = nx.relabel_nodes(fullgraph_with_chemsim, relabel_graph)
+    nx.write_edgelist(fullgraph_with_chemsim_relabeled, DEFAULT_GRAPH_PATH, data=False)
+    pybel.to_pickle(fullgraph_with_chemsim, DEFAULT_FULLGRAPH_PICKLE)
+    pybel.to_pickle(chemsim_graph, DEFAULT_CHEMSIM_PICKLE)
+    return fullgraph_with_chemsim
+
+
+def cluster_chemicals(
+        *,
+        rebuild: bool = False,
+        graph=DEFAULT_FULLGRAPH_WITHOUT_CHEMSIM_PICKLE,
+        mapping_file=DEFAULT_CHEMICALS_MAPPING_PATH,
+):
+    ##TODO: refactor and optimize this code
+    if not rebuild and os.path.exists(DEFAULT_CLUSTERED_CHEMICALS):
+        return pd.read_csv(
+            DEFAULT_CLUSTERED_CHEMICALS,
+            sep="\t",
+            index_col=False,
+        )
+    fullgraph = pybel.from_pickle(graph)
+    chemicals_mapping = pd.read_csv(
+        mapping_file,
+        sep="\t",
+        dtype={'PubchemID': str, 'Smiles': str},
+        index_col=False,
+    )
+    chem_list = []
+    for node in fullgraph.nodes():
+        if node.namespace != 'pubchem.compound':
+            continue
+        chem_list.append(node.identifier)
+    mols_dict = {}
+    for index, row in chemicals_mapping.iterrows():
+        if str(row['PubchemID']) not in chem_list:
+            continue
+        mols_dict[row['PubchemID']] = Chem.MolFromSmiles(row['Smiles'])
+    for chem in tqdm(chem_list):
+        if chem not in mols_dict.keys():
+            smiles = cid_to_smiles(chem)
+            if not isinstance(smiles, str):
+                smiles = smiles.decode("utf-8")
+            if smiles is None:
+                continue
+            mols_dict[chem] = Chem.MolFromSmiles(smiles)
+    fps_drug = {}
+    fps = []
+    drugs = []
+    for drug, mol in tqdm(mols_dict.items()):
+        if mol is None:
+            continue
+        fp = MACCSkeys.GenMACCSKeys(mol)
+        fps.append(fp)
+        drugs.append(drug)
+        fps_drug[drug] = fp
+    dists = []
+    nfps = len(fps)
+    for i in tqdm(range(1, nfps)):
+        sims = DataStructs.BulkTanimotoSimilarity(fps[i], fps[:i])
+        dists.extend([1 - x for x in sims])
+    cs = Butina.ClusterData(dists, nfps, 0.3, isDistData=True)
+    df = pd.DataFrame(columns=['PubchemID', 'Cluster'])
+    i = 1
+    j = 1
+    for cluster in cs:
+        for drug in cluster:
+            df.loc[i] = [drugs[drug - 1]] + [j]
+            i += 1
+        j += 1
+    df.to_csv(DEFAULT_CLUSTERED_CHEMICALS, sep='\t', index=False)
+    return df
